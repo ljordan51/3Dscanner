d013194c48669b2b0bc7aa26e647082f9360a8f7
Rowan Sharman <rsharman@olin.edu>
2017-09-16 17:53:16 -0400

    Arduino code working

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -3,7 +3,12 @@
 const byte panPin = 8;
 const byte tiltPin = 9;
 const byte sensorPin = 3;
-const byte step = 10;
+const byte step = 3;
+const byte panMin = 42;
+const byte panMax = 132;
+const byte tiltMin = 30;
+const byte tiltMax = 120;
+const int delayTime = 180;
 
 Servo pan;
 Servo tilt;
@@ -12,29 +17,54 @@ void setup() {
   Serial.begin(9600);
   pan.attach(panPin, 570, 2380);
   tilt.attach(tiltPin, 550, 2280);
-  pan.write(0);
-  tilt.write(0);
-
+  pan.write((panMin+panMax)/2);
+  tilt.write((tiltMin+tiltMax)/2);
+  delay(1000);
 }
 
 void loop() {
-  delay(700);
-  for(int i = 0; i <= 180; i += step){
-    pan.write(i);
-    delay(700);
-    for(int j = 0; j <= 180; j += step){
-      tilt.write(j);
-      delay(200);
-      dist = analogRead(sensorPin)
-      byte data[6];
-      data[0] = i;
-      data[1] = j;
-      data[2] = dist & 255;
-      data[3] = (dist >> 8)  & 255;
-      data[4] = (dist >> 16) & 255;
-      data[5] = (dist >> 24) & 255;
-      Serial.write(data, 6);
-    }
-    tilt.write(0);
+  while(!Serial.available()){
+    pan.write((panMin+panMax)/2);
+    tilt.write((tiltMin+tiltMax)/2);
   }
+  while(Serial.available()){
+    Serial.read();
+    delay(5);
+  }
+
+  pan.write(panMin);
+  tilt.write(tiltMin);
+  delay(700);
+  bool scanDir = 1;
+  for(byte i = panMin; i <= panMax; i += step){
+    pan.write(i);
+    delay(delayTime);
+    if(scanDir){
+      for(byte j = tiltMin; j <= tiltMax; j += step){
+        tilt.write(j);
+        delay(delayTime);
+        measureSend(i, j);
+      }
+    }
+    else{
+      for(int j = tiltMax; j >= tiltMin; j -= step){
+        tilt.write(j);
+        delay(delayTime);
+        measureSend(i, j);
+      }
+    }
+    scanDir = !scanDir;
+  }
+}
+
+void measureSend(byte x, byte y){
+  int dist = analogRead(sensorPin);
+  byte data[6];
+  data[0] = x;
+  data[1] = y;
+  data[2] = dist & 255;
+  data[3] = (dist >> 8)  & 255;
+  data[4] = (dist >> 16) & 255;
+  data[5] = (dist >> 24) & 255;
+  Serial.write(data, 6);
 }
