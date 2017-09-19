004391039838ce88367d989593b1899e457f974e
ljordan51 <ljordan51@gmail.com>
2017-09-19 10:41:34 -0400

    matlab reads serial data from arduino, not sure if numbers are correct, need to make more edits

--- readSerial.m
+++ readSerial.m
@@ -1,9 +1,22 @@
-s = serial('COM4', 'BaudRate', 9600);
+s = serial('COM7', 'BaudRate', 9600);
+%s.InputBufferSize = 6;
 disp(s);
+A = zeros(6,1);
+temp = zeros(6,1)-1;
 
 fopen(s);
-fread(s)
-%data=fscanf(s)
-%y=str2double(data);
-%disp(y);
+fwrite(s,1);
+while 1
+    if s.BytesAvailable == 6
+        temp = fread(s);
+    end
+    if temp(1) == 255
+        break
+    elseif temp(1) == -1
+        continue
+    else
+        A = [A;temp];
+    end
+end
 fclose(s);
+disp('done');
