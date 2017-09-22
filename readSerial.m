3a3bc88f446a07edce3e5ff2e74d5c27adb41a8b
ljordan51 <ljordan51@gmail.com>
2017-09-22 10:11:28 -0400

    still working on arduino code, need to figure out why we can only read 6 bytes

--- readSerial.m
+++ readSerial.m
@@ -1,22 +1,24 @@
 s = serial('COM7', 'BaudRate', 9600);
-%s.InputBufferSize = 6;
+s.InputBufferSize = 60;
 disp(s);
-A = zeros(6,1);
-temp = zeros(6,1)-1;
+% A = zeros(6,1);
+% temp = zeros(6,1)-1;
 
 fopen(s);
 fwrite(s,1);
-while 1
-    if s.BytesAvailable == 6
-        temp = fread(s);
-    end
-    if temp(1) == 255
-        break
-    elseif temp(1) == -1
-        continue
-    else
-        A = [A;temp];
-    end
-end
+pause(20);
+% while 1
+%     if s.BytesAvailable == 6
+%         temp = fread(s);
+%     end
+%     if temp(1) == 255
+%         break
+%     elseif temp(1) == -1
+%         continue
+%     else
+%         A = [A;temp];
+%     end
+% end
+disp(s);
 fclose(s);
 disp('done');
